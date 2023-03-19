5516e387564b58c631f50a4670b0e4a739d65258
IvanCherniienko <111380678+IvanCherniienko@users.noreply.github.com>
2023-03-19 18:14:28 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const postBlock = document.querySelector('.js-post-div')
 const commentsButton = document.querySelector('.js-comm-btn')
 const commentsBlock = document.querySelector('.js-comments-div')
 
-goBtn.addEventListener('click', function() {
+async function getPost(postId) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  if (!response.status == 200) {
+    alert('Error status')
+  }
+  const post = await response.json()
+  return post
+}
+
+async function getComments(postId) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+  if (!response.status == 200) {
+    alert('Error')
+  }
+  const comments = await response.json()
+  return comments
+}
+
+goBtn.addEventListener('click', async function() {
   const postId = inputId.value
 
   if (postId < 1 || postId > 100) {
@@ -12,46 +30,29 @@ goBtn.addEventListener('click', function() {
     return
   }
 
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error status')
+  try {
+    const post = await getPost(postId)
+    const postHtml = `<h2>${post.title}</h2><p>${post.body}</p>`
+    postBlock.innerHTML = postHtml
+
+    commentsButton.classList.remove("js-comm-btn")
+    commentsButton.classList.add("block-open")
+
+    commentsButton.addEventListener('click', async function() {
+      try {
+        const comments = await getComments(post.id)
+
+        let commentsHtml = '<ul>'
+        comments.forEach(comment => {
+          commentsHtml += `<li>${comment.body}</li>`
+        })
+        commentsHtml += '</ul>'
+        commentsBlock.innerHTML = commentsHtml
+      } catch (error) {
+        alert(error.message)
       }
-      return response.json()
     })
-    .then(post => {
-      const postHtml = `<h2>${post.title}</h2><p>${post.body}</p>`
-      postBlock.innerHTML = postHtml
-      commentsButton.classList.remove("js-comm-btn")
-      commentsButton.classList.add("block-open")
-      commentsButton.addEventListener('click', function() {
-        fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Error')
-            }
-            return response.json()
-          })
-          .then(comments => {
-            let commentsHtml = '<ul>'
-            comments.forEach(comment => {
-              commentsHtml += `<li>${comment.body}</li>`
-            })
-            commentsHtml += '</ul>'
-            commentsBlock.innerHTML = commentsHtml
-          })
-          .catch(error => {
-            alert(error.message)
-          })
-      })
-    })
-    .catch(error => {
-      alert(error.message)
-    })
+  } catch (error) {
+    alert(error.message)
+  }
 })
-
-
-
-
-
-
